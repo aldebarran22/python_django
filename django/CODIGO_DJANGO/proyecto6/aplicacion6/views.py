27ff92e988831bbc6c032b8e3476c1ecd45ec929
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-08-01 21:30:44 +0200

    martes_todo

--- django/CODIGO_DJANGO/proyecto6/aplicacion6/views.py
+++ django/CODIGO_DJANGO/proyecto6/aplicacion6/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from aplicacion6.models import Author
+
+def index(request):
+    # Accedemos al modelo:
+    numAutores = Author.objects.count()
+
+    # Cargamos la información en el contexto y pasamos el contexto al template
+    contexto = {"numAutores":numAutores}
+    return render(request, 'index.html', contexto)
--- django/CODIGO_DJANGO/proyecto6/proyecto6/urls.py
+++ django/CODIGO_DJANGO/proyecto6/proyecto6/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import aplicacion6.views
+
 urlpatterns = [
+    path('', aplicacion6.views.index),
     path('admin/', admin.site.urls),
 ]
