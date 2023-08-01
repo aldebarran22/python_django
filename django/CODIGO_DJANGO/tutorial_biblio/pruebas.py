6b670320da932e89399a7343d3a9e4f6854e4c98
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-08-01 17:59:44 +0200

    martes_18_00

--- django/CODIGO_DJANGO/tutorial_biblio/pruebas.py
+++ django/CODIGO_DJANGO/tutorial_biblio/pruebas.py
@@ -1,7 +1,7 @@
 
-from tutorial.models import Book, Author
+from tutorial.models import Book, Author, Publisher
 from random import randint
-from django.db.models import Avg, Sum, Min, Max
+from django.db.models import Avg, Sum, Min, Max, Count
 
 def main():
     # Crear un autor con cada nombre
@@ -25,5 +25,12 @@ def consultasAgregacion():
     print('Suma del precio de los libros de cobol:', d3)
 
 
-
+def consultasGrupos():
+    # Consultas con annotate, opera a nivel de grupos
+    # Hace grupos y por cada grupo aplica una funcion
+    # de agregado.
+    L = Publisher.objects.annotate(num_books=Count('book'))
+    print('Num libros por editor:')
+    for obj in L:
+        print(obj.name, 'num_libros: ', obj.num_books)
 
