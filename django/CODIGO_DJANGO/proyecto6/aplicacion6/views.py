45976d5c8f0dd6adcc2eb75bd58197a193cf16e3
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-08-02 17:17:49 +0200

    mier_17_17

--- django/CODIGO_DJANGO/proyecto6/aplicacion6/views.py
+++ django/CODIGO_DJANGO/proyecto6/aplicacion6/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from aplicacion6.models import Author
+from aplicacion6.models import Author, Book
+from django.db.models import Sum
 
 def index(request):
     # mostrar la página principal con unos enlaces:
     return render(request, 'index.html')
 
 def autores(request):
-    # Accedemos al modelo:
+    # Accedemos al modelo: Recuento de autores:
     numAutores = Author.objects.count()
 
+    # Precio total de los libros:
+    d = Book.objects.aggregate(precio_total=Sum('price'))  
+    #print(d) 
+
+    # Recuperar todos los libros:
+    libros = Book.objects.all().order_by("-price")
+
     # Cargamos la información en el contexto y pasamos el contexto al template
-    contexto = {"numAutores":numAutores}
+    contexto = {"numAutores":numAutores, "totalLibros":d, "libros":libros}
     return render(request, 'autores.html', context=contexto)
 
