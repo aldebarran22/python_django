ba994e08b7e451aa596005504540652b8903ab07
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 20:18:31 +0100

    20.18

---
+++ codigo_python_2024/control_flujo.py
@@ -0,0 +1,11 @@
+"""
+Bucles: for, while
+Condiciones: if else, if compacto
+break y continue
+"""
+
+M = [[1,2,3],[4,5,6],[7,8,9]]
+for fila in M:
+    for i in fila:
+        print(i, end="\t")
+    print()
