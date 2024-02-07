40d0c716d2402656fd18e1d312b038f4ea778185
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 20:24:08 +0100

    20.24

--- codigo_python_2024/control_flujo.py
+++ codigo_python_2024/control_flujo.py
@@ -4,8 +4,21 @@ Condiciones: if else, if compacto
 break y continue
 """
 
-M = [[1,2,3],[4,5,6],[7,8,9]]
+# Bucles anidados para imprimir una matriz
+M = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 for fila in M:
     for i in fila:
         print(i, end="\t")
     print()
+
+# Dados 2 números comprobar si son iguales, o cual es mayor o menor
+n1 = 10
+n2 = 20
+
+if n1 < n2:
+    print("El menor es: ", n1)
+
+elif n1 == n2:
+    print("son iguales")
+else:
+    print("El mayor es: ", n1)
