288b8fe40985ca536c7b1002b6093651499f1bca
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 19:22:31 +0100

    19.22

--- codigo_python_2024/practica1.py
+++ codigo_python_2024/practica1.py
@@ -8,6 +8,13 @@ de veces que se repite cada palabra en texto
 fich = open("Contenido.txt","r")
 txt = fich.read()
 fich.close()
-
+# Borrar separadores del texto
 txt = txt.replace(",","").replace(".","").replace(";","").replace(":","")
 print(txt)
+
+palabras = txt.split(" ")
+print('num palabras: ', len(palabras))
+
+palabrasSinRepes = set(palabras)
+print('num palabras sin repes: ', len(palabrasSinRepes))
+
