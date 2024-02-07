3ca9a5ba8160c69dd732e667dd0b88cbaa883e6f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 18:41:31 +0100

    18.41

--- codigo_python_2024/diccionarios.py
+++ codigo_python_2024/diccionarios.py
@@ -26,15 +26,32 @@ print(clases)
 
 # range(ini, fin-1, salto=1)
 
-dias = "LMXJVSD"
+dias = "LMXJV"
 numeros = list(range(1,8))
 print(numeros)
 
 d2 = dict(zip(dias, numeros))
+d2['S'] = 6
 print(d2)
 
 d3 = dict(zip(numeros, dias))
 print(d3)
+
+# las claves de d3:
+print(list(d3.keys()))
+# los valores:
+print(list(d3.values()))
+print(d3[3])
+
+try:
+    # Acceso a una clave que no existe: produce la excepción KeyError
+    print(d3[30])
+except Exception as e:
+    print("Se ha producido un error:", e)
+
+print('fin')
+
+
 
 
 
