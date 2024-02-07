6ec4ebc0fcf1b3abe43106dc9911d2251c3cfe73
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 19:09:27 +0100

    19.09

---
+++ codigo_python_2024/practica1.py
@@ -0,0 +1,12 @@
+"""
+Cargar el texto del fichero: Contenido.txt
+Generar un histograma que nos muestre el número
+de veces que se repite cada palabra en texto
+"""
+
+# Cargar el fichero:
+fich = open("Contenido.txt","r")
+txt = fich.read()
+fich.close()
+
+print(txt)
