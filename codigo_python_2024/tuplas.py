7afb63605e3dfec2a1d81f2362a38227ada68031
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 20:09:37 +0100

    20.09

--- codigo_python_2024/tuplas.py
+++ codigo_python_2024/tuplas.py
@@ -4,8 +4,14 @@ Creación, acceso a los elementos, ...
 - Inicializar múltiples
 - Intercambiar variables
 """
-t1 = 1,2,3,4,
-t2 = (5,6,7,8)
+
+t1 = (
+    1,
+    2,
+    3,
+    4,
+)
+t2 = (5, 6, 7, 8)
 print(t1, type(t1))
 print(t2, type(t2))
 t3 = (4,)
@@ -14,8 +20,8 @@ print(t3, type(t3))
 frase = "hola que tal"
 t = tuple(frase)
 print(t)
-print('El espacio ocupa la pos: ', t.index(' '))
-print('El 2º espacio ocupa la pos: ', t.index(' ',5))
+print("El espacio ocupa la pos: ", t.index(" "))
+print("El 2º espacio ocupa la pos: ", t.index(" ", 5))
 try:
     t[0] = "X"
 except Exception as e:
@@ -23,10 +29,14 @@ except Exception as e:
 
 # Imprimir las posiciones que ocupan los blancos en una tupla
 frase = "hola que tal el lunes"
-n = frase.count(' ')
+n = frase.count(" ")
 inicio = 0
 for i in range(n):
-    pos = frase.index(' ', inicio)
-    print('pos blanco:', pos)
-    inicio = pos+1
+    pos = frase.index(" ", inicio)
+    print("pos blanco:", pos)
+    inicio = pos + 1
 
+L = [("Ana", 34, 1.77), ("Alberto", 55, 1.8), ("Eva", 39, 1.9)]
+# Expansión de tuplas:
+for nombre, edad, altura in L:
+    print(nombre)
