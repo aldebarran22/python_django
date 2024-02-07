5f3868ace9205249c5f25060ae604571f6b15dd7
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 19:48:37 +0100

    19.48

---
+++ codigo_python_2024/tuplas.py
@@ -0,0 +1,20 @@
+"""
+Tuplas en Python
+Creación, acceso a los elementos, ...
+- Inicializar múltiples
+- Intercambiar variables
+"""
+t1 = 1,2,3,4,
+t2 = (5,6,7,8)
+print(t1, type(t1))
+print(t2, type(t2))
+t3 = (4,)
+print(t3, type(t3))
+
+frase = "hola que tal"
+t = tuple(frase)
+print(t)
+try:
+    t[0] = "X"
+except Exception as e:
+    print(e)
