3596330237b1bc624aa30ae3b5b1e7a9f3b5403e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 18:14:30 +0100

    18.14

---
+++ codigo_python_2024/funciones.py
@@ -0,0 +1,13 @@
+"""
+Funciones en python
+"""
+
+def sumar(x,y):
+    """
+    Suma el valor de x e y.
+    """
+    return x+y
+
+if __name__ == '__main__':
+    # EL módulo está en ejecución
+    print('suma de 5 y 6: ', sumar(5,6))
