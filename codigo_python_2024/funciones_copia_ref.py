c72d33f8f0ee33430025c16db70c8d0f8522709f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 19:34:21 +0100

    19.34

--- codigo_python_2024/funciones_copia_ref.py
+++ codigo_python_2024/funciones_copia_ref.py
@@ -5,7 +5,16 @@ Paso de parámetros a funciones:
 """
 
 
-def porReferencia(L: list, d: dict, c: set):
+def porCopia(numero: int) -> None:
+    numero += 100
+
+
+def testInmutables():
+    numero = 5
+    print("numero: ", numero)
+
+
+def porReferencia(L: list, d: dict, c: set) -> None:
     L.append(100)
     d["nuevo"] = 1000
     c.remove(10)
@@ -15,12 +24,17 @@ def porReferencia(L: list, d: dict, c: set):
     print("-" * 20)
 
 
-if __name__ == "__main__":
+def testMutables():
     L = [1, 2, 3, 4, 5]
     d = {"a": 1, "b": 2, "c": 3, "d": 4}
     c = {10, 20, 30, 40}
-    porReferencia(L.copy(), d.copy(), c.copy())
-    # porReferencia(L, d, c)
+    # porReferencia(L.copy(), d.copy(), c.copy())
+    porReferencia(L, d, c)
     print("L", L, id(L))
     print("d: ", d, id(d))
     print("c: ", c, id(c))
+
+
+if __name__ == "__main__":
+    # testMutables()
+    testInmutables()
