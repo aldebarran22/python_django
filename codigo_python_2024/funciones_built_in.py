7d41e6a5360ff45a9d16084b51e962c6d2c49cd1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 20:16:39 +0100

    20.16

---
+++ codigo_python_2024/funciones_built_in.py
@@ -0,0 +1,16 @@
+"""
+Funciones definidas dentro de Python
+"""
+
+def calcularOperaciones(coleccion):
+    print('Tipo: ', type(coleccion))
+    print('suma:', sum(coleccion))
+    print('min:', min(coleccion))
+    print('max:', max(coleccion))
+    print('len:', len(coleccion))
+    print('promedio:', sum(coleccion)/len(coleccion))
+    print()
+
+# Operaciones con una lista:
+L = [1,3,4,5,6,7,8,1]
+calcularOperaciones(L)
