e6c2206a8396c605954a266065440fbd748f101a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 18:59:55 +0100

    19.00

--- codigo_python_2024/funciones_param.py
+++ codigo_python_2024/funciones_param.py
@@ -1,18 +1,25 @@
 """
 Definición de parámetros en la entrada de una función
 """
+
+
 def funcion(ob1, ob2, op1=1, op2=2, *args, **kwargs):
-    print('obligatorios: ', ob1, ob2)
-    print('opcionales: ', op1, op2)
-    print('args: ', args)
-    print('kwargs: ', kwargs)
+    print("obligatorios: ", ob1, ob2)
+    print("opcionales: ", op1, op2)
+    print("args: ", args)
+    print("kwargs: ", kwargs)
     print()
 
-if __name__ == '__main__':
-    funcion(10,20)
-    funcion(1,2,3,4)
-    funcion(10,20, op2=200)
-    funcion(1,2,3,4,5,6,7,8, x=-1, y=-2)
 
-    
+# Tipos anotados:
+def funcion2(n: int, nombre: str = "hola") -> int:
+    return len(nombre)
 
+
+if __name__ == "__main__":
+    print(funcion2([1, 2, 3, 4, 5]))
+
+    funcion(10, 20)
+    funcion(1, 2, 3, 4)
+    funcion(10, 20, op2=200)
+    funcion(1, 2, 3, 4, 5, 6, 7, 8, x=-1, y=-2)
