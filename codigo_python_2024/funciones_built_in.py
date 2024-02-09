b2b5fc43cf409aef7d73bf9ab6f6b361eabae016
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 20:25:07 +0100

    20.25

--- codigo_python_2024/funciones_built_in.py
+++ codigo_python_2024/funciones_built_in.py
@@ -2,24 +2,30 @@
 Funciones definidas dentro de Python
 """
 
+import math
+
+
 def calcularOperaciones(coleccion):
-    print('Tipo: ', type(coleccion))
-    print('suma:', sum(coleccion))
-    print('min:', min(coleccion))
-    print('max:', max(coleccion))
-    print('len:', len(coleccion))
-    print('promedio:', sum(coleccion)/len(coleccion))
+    print("Tipo: ", type(coleccion))
+    print("suma:", sum(coleccion))
+    print("min:", min(coleccion))
+    print("max:", max(coleccion))
+    print("len:", len(coleccion))
+    print("promedio:", sum(coleccion) / len(coleccion))
     print()
 
+
 # Operaciones con una lista:
-L = [1,3,4,5,6,7,8,1]
+L = [1, 3, 4, 5, 6, 7, 8, 1]
 calcularOperaciones(L)
 
 # Operaciones con una tupla:
-t = (1,3,4,5,6,7,8,1)
+t = (1, 3, 4, 5, 6, 7, 8, 1)
 calcularOperaciones(t)
 
 # Operaciones con un conjunto:
-c = {1,3,4,5,6,7,8,1}
+c = {1, 3, 4, 5, 6, 7, 8, 1}
 calcularOperaciones(c)
 
+# Redondeos:
+print(round(2.356, 2), math.floor(1.9999), math.ceil(1.0000001))
