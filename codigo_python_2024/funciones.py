2625cc2c931b1d31812250b643d82c8f4d259536
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 18:27:10 +0100

    18.27

--- codigo_python_2024/funciones.py
+++ codigo_python_2024/funciones.py
@@ -16,4 +16,9 @@ def sumar3(x,y,z):
 
 if __name__ == '__main__':
     # EL módulo está en ejecución
-    print('suma de 5 y 6: ', sumar(5,6))
+    print('suma de 5 y 6: ', sumar(5,6)) # forma posicional
+    print('suma de 5 y 6: ', sumar(y=6, x=5)) # forma nominal
+    t = (5,6)
+    print('suma de 5 y 6: ', sumar(*t)) # con una tupla
+    L = [5,6]
+    print('suma de 5 y 6: ', sumar(*L)) # con una tupla
