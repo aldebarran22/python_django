c90d276d79290c8981bea20fb033ef477ebae68f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-12 19:38:03 +0100

    19.37

--- codigo_python_2024/funcional2.py
+++ codigo_python_2024/funcional2.py
@@ -7,7 +7,12 @@ from random import randint
 
 # Generar una lista de números aleatorios:
 L = [randint(1,30) for _ in range(20)]
-print(L)
+print(L, len(L))
 
 c = {randint(1,30) for _ in range(20)}
 print(c, len(c))
+
+d = {letra:[randint(1,30) for _ in range(5)] \
+       for letra in "abcde"}
+for k, v in d.items():
+    print(k, v)
