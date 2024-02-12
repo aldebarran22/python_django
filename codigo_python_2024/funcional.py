ac70c0825a38f3517130ea9ee00c25bf9d031c84
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-12 18:59:26 +0100

    18.59

--- codigo_python_2024/funcional.py
+++ codigo_python_2024/funcional.py
@@ -2,8 +2,10 @@
 Programación funcional: map, filter, reduce y list compre.
 """
 
+
 def calcularIva(precio):
-    return round(precio * .21, 2)
+    return round(precio * 0.21, 2)
+
 
 def test1():
     L = [120.0, 45.0, 70.0, 230.0]
@@ -15,11 +17,27 @@ def test1():
     print(L)
     print(R)
 
+
 def testMap():
     L = [120.0, 45.0, 70.0, 230.0]
     R = list(map(calcularIva, L))
     print(L)
     print(R)
+
+
+def menores100(valor):
+    if valor < 100:
+        return True
+    else:
+        return False
+
+
+def testFilter():
+    # Filtrar los precios < 100:
+    L = [120.0, 45.0, 70.0, 230.0, 23.04, 55, 300, 450, 1000, 21.76]
+    R = list(filter(menores100, L))
+    print(R)
+
 
 def testListC():
     L = [120.0, 45.0, 70.0, 230.0]
@@ -29,7 +47,9 @@ def testListC():
     print(R)
     print(R2)
 
-if __name__ == '__main__':
-    test1()
-    testMap()
-    testListC()
+
+if __name__ == "__main__":
+    # test1()
+    # testMap()
+    # testListC()
+    testFilter()
