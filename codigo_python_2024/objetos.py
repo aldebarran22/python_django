02c525646b183c9c8ef018757c78e6d43f9bdff9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-12 20:05:48 +0100

    20.05

--- codigo_python_2024/objetos.py
+++ codigo_python_2024/objetos.py
@@ -12,11 +12,17 @@ class Persona:
         self.peso = peso
         self.altura = altura
 
+    def correr(self):
+        print(self.nombre,'está corriendo')
+
 if __name__ == '__main__':
     p1 = Persona("Ana")
     p2 = Persona("Miguel", altura=1.8)
     p3 = Persona("Eva", 34, 1.82)
-    
+    # objeto.metodo()
+    p1.correr()
+    print(p1)
+
 
     
 
