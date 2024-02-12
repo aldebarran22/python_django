2fe3a5f5ff54b89294b3915ed97bbc57f7ae8f2b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-12 20:11:17 +0100

    20.11

--- codigo_python_2024/objetos.py
+++ codigo_python_2024/objetos.py
@@ -12,17 +12,24 @@ class Persona:
         self.peso = peso
         self.altura = altura
 
+    def __str__(self):
+        return self.nombre+" "+str(self.peso)+" "+str(self.altura)
+    
+    def __repr__(self):
+        return str(self)
+
     def correr(self):
         print(self.nombre,'está corriendo')
 
 if __name__ == '__main__':
-    p1 = Persona("Ana")
-    p2 = Persona("Miguel", altura=1.8)
-    p3 = Persona("Eva", 34, 1.82)
+    p1 = Persona("Ana",66,1.77)
+    p2 = Persona("Miguel",80, altura=1.8)
+    p3 = Persona("Eva", 50, 1.82)
     # objeto.metodo()
     p1.correr()
-    print(p1)
-
+    print(p1) # print(str(p1))  print(p1.__str__())
+    L = [p1, p2, p3]
+    print(L)
 
     
 
