9c8d6ae77ec9fd20999c9f0c992c1d479e7246dd
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-12 19:59:44 +0100

    19.59

---
+++ codigo_python_2024/objetos.py
@@ -0,0 +1,22 @@
+"""
+POO en Python
+"""
+
+class Persona:
+    """
+    Implementación de la clase Persona
+    """
+        
+    def __init__(self, nombre="", peso=0, altura=0.0):
+        self.nombre = nombre
+        self.peso = peso
+        self.altura = altura
+
+if __name__ == '__main__':
+    p1 = Persona("Ana")
+    p2 = Persona("Miguel", altura=1.8)
+    p3 = Persona("Eva", 34, 1.82)
+    
+
+    
+
