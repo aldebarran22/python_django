648bc23eccd72c3c985858222c94d28f26e25dbb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-14 19:54:04 +0100

    19.54

--- codigo_python_2024/herencia.py
+++ codigo_python_2024/herencia.py
@@ -16,4 +16,12 @@ from objetos import Persona
 
 
 class Empleado(Persona):
-    pass
+    
+    def __init__(self,nombre="", peso=0, altura=0.0, empresa="",sueldo=0.0):
+        # Llamar al constructor de la clase Padre:
+        Persona.__init__(self,nombre, peso, altura)
+
+        # Definir los atributos de la clase Empleado:
+        self.empresa = empresa
+        self.sueldo = sueldo
+
