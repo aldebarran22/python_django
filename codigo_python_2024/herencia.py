33d0b5e096ea0a13f34fb2f1c0e4acab29b762bd
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-14 20:29:19 +0100

    20.30

--- codigo_python_2024/herencia.py
+++ codigo_python_2024/herencia.py
@@ -28,16 +28,22 @@ class Empleado(Persona):
     def __str__(self):
         return Persona.__str__(self) + " " + self.empresa + " " + str(self.sueldo)
 
-    
+    """
     def __lt__(self, other):
         return self.sueldo < other.sueldo
-   
+    """
+
+
 class Programador(Empleado):
 
-    def __init__(self)
+    def __init__(
+        self, nombre="", peso=0, altura=0.0, empresa="", sueldo=0.0, lenguajes=[]
+    ):
+        Empleado.__init__(self, nombre, peso, altura, empresa, sueldo)
+        self.lenguajes = lenguajes
 
 
-if __name__ == "__main__":
+def testEmpleado():
     emp = Empleado("Sara", 60, 1.8, "CAS", 2000.0)
     emp2 = Empleado("Raquel", 60, 1.79, "TRT", 2500.0)
     if emp < emp2:
@@ -45,9 +51,22 @@ if __name__ == "__main__":
     else:
         print(emp2.nombre, "es menor", emp.nombre)
 
-    exit()
     print(emp.__dict__)
     # del(emp.__dict__['peso'])
     emp.__dict__["tno"] = 696558877
     emp.correr()
     print(emp.__dict__)
+
+
+def testProgramador():
+    pg1 = Programador("Sara", 60, 1.8, "CAS", 2000.0, ["python", "Java"])
+    pg2 = Programador("Raquel", 60, 1.79, "TRT", 2500.0, ["python", "C++"])
+    if pg1 < pg2:
+        print(pg1.nombre, "es menor", pg2.nombre)
+    else:
+        print(pg2.nombre, "es menor", pg1.nombre)
+
+
+if __name__ == "__main__":
+    # testEmpleado()
+    testProgramador()
