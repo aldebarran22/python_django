d6216e82091faf808178a9ad85e35726c34f9c08
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-14 19:02:25 +0100

    19.02

--- codigo_python_2024/ordenaciones.py
+++ codigo_python_2024/ordenaciones.py
@@ -2,9 +2,18 @@
 Ordenar colecciones en Python. Funciones lambda
 """
 
+
+def ordenar(t):
+    pass
+
+
 # Ordenar una lista de tuplas:
-L = [("Sara", 24, 1.7), ("Raúl", 44, 1.79), ("Eva", 56, 1.88), ("Gema", 35, 1.78)]
+L = [("Sara", 44, 1.9), ("Raúl", 44, 1.79), ("Eva", 56, 1.88), ("Gema", 56, 1.78)]
 L.sort()  # Por defecto ordena por el primer campo de la tupla (str)
 print(L)
 L.sort(key=lambda t: t[1])
 print(L)
+# Ordenar por edad y altura
+L.sort(key=lambda t: (t[1], t[2]))
+print(L)
+# L = [(12,30,5),(9,36,0),(12,8,34)]
