64733a8982a62127d070ca96069590a8548e42c4
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2024-02-15 19:00:26 +0100

    18.59

--- codigo_python_2024/excepciones.py
+++ codigo_python_2024/excepciones.py
@@ -12,6 +12,19 @@ def test1():
     except IndexError as e:
         print(e.__class__.__name__, e)
 
+def test2():
+    # Capturar varias excepciones:
+    try:
+        n1 = 10
+        d = {"a": 1, "b": 2, "c": 3}
+        print(n1 / 10)
+        print(d['z'])
+    except KeyError as e:
+        print(e.__class__.__name__, e)
+    except ZeroDivisionError as e:
+        print(e.__class__.__name__, e)
+
 
 if __name__ == "__main__":
-    test1()
+    #test1()
+    test2()
