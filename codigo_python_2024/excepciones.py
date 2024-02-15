b15a7425018c84e80b0ac82a018be70bcf230ee6
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2024-02-15 19:05:29 +0100

    Add files via upload

--- codigo_python_2024/excepciones.py
+++ codigo_python_2024/excepciones.py
@@ -12,19 +12,32 @@ def test1():
     except IndexError as e:
         print(e.__class__.__name__, e)
 
+
 def test2():
     # Capturar varias excepciones:
     try:
         n1 = 10
         d = {"a": 1, "b": 2, "c": 3}
         print(n1 / 10)
-        print(d['z'])
+        print(d["z"])
     except KeyError as e:
         print(e.__class__.__name__, e)
     except ZeroDivisionError as e:
         print(e.__class__.__name__, e)
 
 
+def test3():
+    # Agrupar varias excepciones en una tupla:
+    try:
+        n1 = 10
+        d = {"a": 1, "b": 2, "c": 3}
+        print(n1 / 10)
+        print(d["z"])
+    except (KeyError, ZeroDivisionError) as e:
+        print(e.__class__.__name__, e)
+
+
 if __name__ == "__main__":
-    #test1()
-    test2()
+    # test1()
+    # test2()
+    test3()
