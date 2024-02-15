f439efabf02494eb8e82099733214fee7e0cb3c4
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2024-02-15 19:44:00 +0100

    19.42

---
+++ codigo_python_2024/aplicacion/principal.py
@@ -0,0 +1,10 @@
+"""
+Estructura de los módulos:
+principal.py
+modulos <dir>
+    __init__.py
+    clase_time.py       ==> class Time
+    clase_date.py       ==> class Date
+    clase_date_time.py  ==> class DateTime
+
+"""
