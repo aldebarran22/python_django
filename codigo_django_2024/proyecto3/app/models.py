a5cd6f5ad9ecc09fa33b5dd3c0ea8bc366a2655c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-21 18:54:16 +0100

    18.54

---
+++ codigo_django_2024/proyecto3/app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+# Create your models here.
+class Autor(models.Model):
+    # Los campos del autor
+    nombre = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=50)
+    correo = models.EmailField(max_length=80)
+
+
+class Editor(models.Model):
+    # Los campos del editor
+    nombre = models.CharField(max_length=30)
+    domicilio = models.CharField(max_length=30)  ###
+    ciudad = models.CharField(max_length=30)
+    provincia = models.CharField(max_length=30)
+    pais = models.CharField(max_length=30)
+    web = models.URLField(max_length=50)
+
+class Libro(models.Model):
+    # Los campos del libro
+    isbn = models.CharField(max_length=20)
+    titulo = models.CharField(max_length=30)
+    precio = models.FloatField()
+
+    # Relaciones:
+    # 1 Editor -> N Libros (de 1 a N)
+    editor = models.ForeignKey(Editor, on_delete=models.PROTECT)
+
+    # N Autores <-> M Libros (de muchos a muchos)
+    autores = models.ManyToManyField(Autor)
