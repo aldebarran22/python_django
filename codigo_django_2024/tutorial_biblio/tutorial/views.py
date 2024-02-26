8e2d10599d85822a60867c676558d6cd116902f3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-26 19:34:03 +0100

    19.33

--- codigo_django_2024/tutorial_biblio/tutorial/views.py
+++ codigo_django_2024/tutorial_biblio/tutorial/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from datetime import datetime
+
+
+def index(request):
+    horaActual = datetime.now()
+    ahora = horaActual.strftime("%d/%m/%Y %H:%M:%S")
+    contexto = {"ahora": ahora}
+    return render(request, "index.html", contexto)
+
+
+
+def saludar(request):
+    print("Ha solicitado saludar")
+    return HttpResponse("<h3>Página de saludo</h3>")
+
+
+def buscarLibro(request, id):
+    print("id:", id)
+    return HttpResponse(f"<h3>el id es: {id}</h3>")
--- codigo_django_2024/tutorial_biblio/tutorial_biblio/urls.py
+++ codigo_django_2024/tutorial_biblio/tutorial_biblio/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+import tutorial.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", tutorial.views.index, name="inicio"),
+    path("hola", tutorial.views.saludar, name="saludo"),
+    path("libros/<int:id>", tutorial.views.buscarLibro),
 ]
