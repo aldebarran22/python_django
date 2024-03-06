0d502be59916c72ebde7246bd6a17e9ea93dc36c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-06 20:08:41 +0100

    20.08

--- codigo_django_2024/tutorial_biblio_plantillas/tutorial/forms.py
+++ codigo_django_2024/tutorial_biblio_plantillas/tutorial/forms.py
@@ -1,7 +1,26 @@
 from django import forms
+from datetime import date
 
 
 class FormUsuario(forms.Form):
     login = forms.CharField()
     # password = forms.PasswordField()
     email = forms.EmailField(required=False)
+
+
+class FormContacto(forms.Form):
+    paises = [(1, "España"), (2, "Italia"), (3, "Francia")]
+
+    nombre = forms.CharField(max_length=25)
+    usuario = forms.CharField(min_length=10)
+    password = forms.CharField(
+        widget=forms.PasswordInput,
+        error_messages={"required": "El password obligatorio"},
+    )
+    email = forms.CharField(widget=forms.EmailInput(), required=False)
+    edad = forms.IntegerField(min_value=18, max_value=99)
+    # Mostrará la fecha inicializada al día de hoy
+    fecha = forms.DateField(initial=date.today())
+    publicidad = forms.BooleanField(required=False, label="Recibir publicidad")
+    pais = forms.ChoiceField(choices=paises, required=False)
+    observaciones = forms.CharField(max_length=200, widget=forms.Textarea)
