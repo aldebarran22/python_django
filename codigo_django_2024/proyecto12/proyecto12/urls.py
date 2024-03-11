7af5ed032bdc1d945ae019c348a3390995f55fe5
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-11 20:31:52 +0100

    todo

--- codigo_django_2024/proyecto12/proyecto12/urls.py
+++ codigo_django_2024/proyecto12/proyecto12/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.contrib.auth.views import LoginView, LogoutView, TemplateView
@@ -24,18 +25,41 @@ from django.contrib.auth.decorators import login_required, permission_required
 import aplicacion12.views
 
 urlpatterns = [
-    path('admin/doc/', include('django.contrib.admindocs.urls')),
-    path('admin/', admin.site.urls),    
-    re_path('^accounts/login/$', LoginView.as_view(), name='login'),    
-    re_path('^accounts/logout/$', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    re_path('^accounts/profile/$', aplicacion12.views.getInfoUser, name='profile'),
-    path('', aplicacion12.views.getInfoUser, name='getinfo'),
-    path('operacion1/', aplicacion12.views.operacion1, name='operacion1'),
-    path('operacion2/', aplicacion12.views.operacion2, name='operacion2'),
-    path('pagina1/', login_required(TemplateView.as_view(template_name='aplicacion12/pagina1.html')), name='pagina1'),
-    path('escuchar_cancion/', permission_required('model_user.escuchar_cancion')(TemplateView.as_view(template_name='aplicacion12/escuchar_cancion.html')), name='escuchar_cancion'),
-    path('bajar_cancion/', permission_required('model_user.bajar_cancion')(TemplateView.as_view(template_name='aplicacion12/bajar_cancion.html')), name='bajar_cancion'),
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
+    path("admin/", admin.site.urls),
+    re_path("^accounts/login/$", LoginView.as_view(), name="login"),
+    re_path(
+        "^accounts/logout/$",
+        LogoutView.as_view(
+            template_name="registration/logout.html", next_page="/accounts/profile"
+        ),
+        name="logout",
+    ),
+    # re_path('^accounts/logout/$', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    re_path("^accounts/profile/$", aplicacion12.views.getInfoUser, name="profile"),
+    path("", aplicacion12.views.getInfoUser, name="getinfo"),
+    path("operacion1/", aplicacion12.views.operacion1, name="operacion1"),
+    path("operacion2/", aplicacion12.views.operacion2, name="operacion2"),
+    path(
+        "pagina1/",
+        login_required(TemplateView.as_view(template_name="aplicacion12/pagina1.html")),
+        name="pagina1",
+    ),
+    path(
+        "escuchar_cancion/",
+        permission_required("model_user.escuchar_cancion")(
+            TemplateView.as_view(template_name="aplicacion12/escuchar_cancion.html")
+        ),
+        name="escuchar_cancion",
+    ),
+    path(
+        "bajar_cancion/",
+        permission_required("model_user.bajar_cancion")(
+            TemplateView.as_view(template_name="aplicacion12/bajar_cancion.html")
+        ),
+        name="bajar_cancion",
+    ),
 ]
 
 # Con esta NO para en la página de logout
-#re_path('^accounts/logout/$', LogoutView.as_view(template_name='registration/logout.html',next_page='/accounts/profile'), name='logout'),
+# re_path('^accounts/logout/$', LogoutView.as_view(template_name='registration/logout.html',next_page='/accounts/profile'), name='logout'),
