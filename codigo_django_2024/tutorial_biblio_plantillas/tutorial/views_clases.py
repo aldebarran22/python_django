c45ef3ded199fedb62e40994f8dda1dd529fe05f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-11 18:09:42 +0100

    18.09

--- codigo_django_2024/tutorial_biblio_plantillas/tutorial/views_clases.py
+++ codigo_django_2024/tutorial_biblio_plantillas/tutorial/views_clases.py
@@ -5,7 +5,7 @@ Vistas basadas en clases
 from tutorial.models import Book
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import DeleteView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
 
 
 class ListadoLibros(ListView):
@@ -20,3 +20,13 @@ class BookDelete(DeleteView):
     model = Book
     fields = ["isbn", "name"]
     success_url = reverse_lazy("libros2")
+
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ["isbn", "name", "price", "publisher", "authors"]
+
+
+class BookCreate(CreateView):
+    model = Book
+    fields = ["isbn", "name", "price", "publisher", "authors"]
